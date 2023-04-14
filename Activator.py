ca289d16c311abf54f854e088eb5ddb26b5bc3f6
Mariiann12 <92766777+Mariiann12@users.noreply.github.com>
2023-04-14 10:25:48 +0100

    Add files via upload

--- Activator.py
+++ Activator.py
@@ -4,6 +4,18 @@ from tkinter import *
 from pygame import mixer
 
 
+window = Tk()
+window.title("")
+window.geometry('350x150')
 
 
+window.mainloop()
 
+
+def sound_alarm():
+    mixer.music.load('alarm_Medication.mp3')
+    mixer.music.play()
+
+mixer.init()
+
+window.mainloop()
